312e2c5cb3e0bf2a2a01c3bb943700c287d71677
commit user <10530777@mydbs.ie>
2020-01-06 02:05:38 +0530

    footer template created

--- home/home/urls.py
+++ home/home/urls.py
@@ -6,6 +6,8 @@ from .import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls), # admin should be start of the string. ^ means that. $ sign mean s end of the string. 
     url(r'^$', views.home), # home page # start and end mentioned by carat and dollar consequtively
+    url(r'^home/$', views.home), # home page # start and end mentioned by carat and dollar consequtively
     url(r'^about/$', views.about), # about page
        url(r'^facilities/', include('facilities.urls')), # about page
+       url(r'^footer/$', views.footer), # about page
 ]
--- home/home/views.py
+++ home/home/views.py
@@ -10,3 +10,7 @@ def about (request):
     return render(request,'aboutPage.html')
 
     #return HttpResponse('babout')
+
+    
+def footer (request):
+    return render(request,'footer.html')
